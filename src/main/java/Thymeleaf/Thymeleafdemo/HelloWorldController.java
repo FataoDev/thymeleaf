740518a13e1a7f84bf609937e09755e02a3210e1
Fatao OUEDRAOGO <fataoouedraogo226@gmail.com>
2023-11-05 13:56:38 +0000

    Début thymeleaf

---
+++ src/main/java/Thymeleaf/Thymeleafdemo/HelloWorldController.java
@@ -0,0 +1,21 @@
+package Thymeleaf.Thymeleafdemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String hello(Model model)
+    {
+        model.addAttribute("message","Hello World");
+        return "helloworld";
+    }
+
+    @GetMapping("/index")
+    public String style()
+    {
+     return "index";
+    }
+}
