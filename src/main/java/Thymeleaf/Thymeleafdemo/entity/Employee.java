2444df90c2e945e48811dd35d13db7a720a9f9ea
Fatao OUEDRAOGO <fataoouedraogo226@gmail.com>
2023-11-06 03:01:58 +0000

    Création des entités dans la base de donnée
    Signed-off-by: Fatao OUEDRAOGO <fataoouedraogo226@gmail.com>

---
+++ src/main/java/Thymeleaf/Thymeleafdemo/entity/Employee.java
@@ -0,0 +1,61 @@
+package Thymeleaf.Thymeleafdemo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int id;
+    private String name;
+    private String email;
+    private String sexe;
+    private String passsword;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSexe() {
+        return sexe;
+    }
+
+    public void setSexe(String sexe) {
+        this.sexe = sexe;
+    }
+
+    public String getPasssword() {
+        return passsword;
+    }
+
+    public void setPasssword(String passsword) {
+        this.passsword = passsword;
+    }
+
+
+}
